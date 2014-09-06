c86c3559174c7a1485471061d42938d30589cf50
Protoneer <krugercreations@gmail.com>
2014-09-07 08:06:57 +1200

    # Changed the API version prefix to look nice

--- app/mod_api_1_0/blueprint.py
+++ app/mod_api_1_0/blueprint.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-mod_api = Blueprint('api', __name__, url_prefix = '/api_1_0')
+mod_api = Blueprint('api', __name__, url_prefix = '/api/v1.0')
 
 @mod_api.route('/', methods=['GET', 'POST'])
 @mod_api.route('/getdata', methods=['GET', 'POST'])
